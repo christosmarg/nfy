2ea859e1c31d6071fd9597628f53439b46b679a4
Christos Margiolis <christos@margiolis.net>
2021-01-27 01:52:12 +0200

    added Xrandr(3) support to handle multiple screens, improved padding window sizes

--- config.h
+++ config.h
@@ -1,11 +1,14 @@
+/* See LICENSE file for copyright and license details. */
 enum { TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT }; /* Window positions */
 static const char *bgcol = "#453588";           /* Background color */
 static const char *bordercol = "#282828";       /* Border color */
 static const char *fontcol = "#ebdbb2";         /* Font color */
 static const char *fonts = "monospace:size=15"; /* Font */
-static const int borderw = 2;                   /* Border width */
+static const int padding = 15;                  /* Padding (pixels) */
+static const int borderw = 2;                   /* Border width (pixels) */
 static const int duration = 3;                  /* Notification duration (seconds) */
 static const int pos = TOP_RIGHT;               /* Window position */
-static const int mx = 10;                       /* Margin X */
-static const int my = 25;                       /* Margin Y */
+static const int mx = 10;                       /* Margin X (pixels) */
+static const int my = 25;                       /* Margin Y (pixels) */
 static const int maxlen = 250;                  /* Max window length (pixels) */
+static const int linespace = 10;                /* Line spacing (pixels) */
--- config.mk
+++ config.mk
@@ -8,23 +8,23 @@ MAN_DIR = ${PREFIX}/share/man/man1
 BIN_DIR = ${PREFIX}/bin
 
 # includes and libs
-#X11INC = /usr/local/include
-#X11LIB = /usr/local/lib
+X11INC = /usr/local/include
+X11LIB = /usr/local/lib
 
 # Linux
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+#X11INC = /usr/X11R6/include
+#X11LIB = /usr/X11R6/lib
 
 FREETYPELIBS = -lfontconfig -lXft
-#FREETYPEINC = ${X11INC}/freetype2
+FREETYPEINC = ${X11INC}/freetype2
 # Linux
-FREETYPEINC = /usr/include/freetype2
+#FREETYPEINC = /usr/include/freetype2
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L \
 	   -D_XOPEN_SOURCE=700 -DVERSION=\"${VERSION}\"
 CFLAGS = -std=c99 -pedantic -Wall -Os -I${X11INC} -I${FREETYPEINC} ${CPPFLAGS}
-LDFLAGS = -L${X11LIB} ${FREETYPELIBS} -lX11
+LDFLAGS = -L${X11LIB} ${FREETYPELIBS} -lX11 -lXrandr
 
 # utils
 CP = cp -f
