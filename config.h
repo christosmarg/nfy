db2c03b9dda742d584185290a66913910b223bd2
Christos Margiolis <christos@margiolis.net>
2021-05-24 20:45:29 +0300

    Fixed lockfile bug, made Makefile portable

--- config.h
+++ config.h
@@ -1,16 +1,21 @@
 /* See LICENSE file for copyright and license details. */
+#ifndef _NFY_CONFIG_H_
+#define _NFY_CONFIG_H_
+
 enum { TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT }; /* Window positions */
 static const char *bgcolor = "#201f1c";		/* Background color */
-static const char *bordercolor = "#f28f19";	/* Border color */
+static const char *bordercolor = "#f18f19";	/* Border color */
 static const char *fontcolor = "#e6e5e3";	/* Font color */
 /* TODO: add fallbacks */
-static const char *fonts = "monospace:size=15"; /* Font */
-static const int padding = 15;			/* Padding (pixels) */
-static const int borderw = 2;			/* Border width (pixels) */
-static const int duration = 3;			/* Notification duration (seconds) */
-static const int pos = TOP_RIGHT;		/* Window position */
-static const int mx = 10;			/* Margin X (pixels) */
-static const int my = 25;			/* Margin Y (pixels) */
-static const int maxlen = 250;			/* Max window length (pixels) */
-static const int linespace = 10;		/* Line spacing (pixels) */
+static const char *fonts = "monospace:size=12"; /* Fonts */
+static const unsigned int padding = 15;		/* Padding (pixels) */
+static const unsigned int borderw = 2;		/* Border width (pixels) */
+static const unsigned int duration = 3;		/* Notification duration (seconds) */
+static const unsigned int pos = TOP_RIGHT;	/* Window position */
+static const unsigned int mx = 10;		/* Margin X (pixels) */
+static const unsigned int my = 25;		/* Margin Y (pixels) */
+static const unsigned int maxlen = 250;		/* Max window length (pixels) */
+static const unsigned int linespace = 10;	/* Line spacing (pixels) */
 static const char *lockfile = "/tmp/nfy.lock";
+
+#endif /* _NFY_CONFIG_H_ */
--- config.mk
+++ config.mk
@@ -1,17 +1,17 @@
 # See LICENSE file for copyright and license details.
 # nfy version
-VERSION = 0
+VERSION = 0.1
 
 # paths
 PREFIX = /usr/local
-MAN_DIR = ${PREFIX}/share/man/man1
-BIN_DIR = ${PREFIX}/bin
+MANPREFIX = ${PREFIX}/share/man
 
 # includes and libs
+# FreeBSD
 X11INC = /usr/local/include
 X11LIB = /usr/local/lib
 
-# Linux
+# Linux/OpenBSD
 #X11INC = /usr/X11R6/include
 #X11LIB = /usr/X11R6/lib
 
@@ -25,16 +25,6 @@ CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L \
 	   -D_XOPEN_SOURCE=700 -DVERSION=\"${VERSION}\"
 CFLAGS = -std=c99 -pedantic -Wall -Os -I${X11INC} -I${FREETYPEINC} ${CPPFLAGS}
 LDFLAGS = -L${X11LIB} ${FREETYPELIBS} -lX11 -lXrandr
-
-# utils
-CP = cp -f
-RM = rm -f
-RM_DIR = rm -rf
-MV = mv
-MKDIR = mkdir -p
-RM_DIR = rm -rf
-TAR = tar -cf
-GZIP = gzip
 
 # compiler
 CC = cc
