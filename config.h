3cb1da6eee93e7ff11ecccdfa0cd2aeab95675f0
Christos Margiolis <christos@margiolis.net>
2022-04-10 17:52:18 +0300

    added duration flag

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const char *fontcolor = "#e6e5e3";	/* Font color */
 static const char *fonts = "monospace:size=12"; /* Fonts */
 static const unsigned int padding = 10;		/* Padding (pixels) */
 static const unsigned int borderw = 3;		/* Border width (pixels) */
-static const unsigned int duration = 3;		/* Notification duration (seconds) */
+static unsigned int duration = 3;		/* Notification duration (seconds) */
 static const unsigned int pos = TOP_RIGHT;	/* Window position */
 static const unsigned int mx = 10;		/* Margin X (pixels) */
 static const unsigned int my = 25;		/* Margin Y (pixels) */
